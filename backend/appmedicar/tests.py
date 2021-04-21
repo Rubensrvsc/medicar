55097052f3f2b8852628de3984d6c733fda2b914
rubensrvsc <Rubensspfc100@gmail.com>
2021-04-21 10:11:36 -0300

    test: add test of endpoint medicos/

--- backend/appmedicar/tests.py
+++ backend/appmedicar/tests.py
@@ -12,3 +12,7 @@ class TesteMedicar(TestCase):
     def test_especialidades(self):
         request = requests.get('http://localhost:8000/especialidades/')
         self.assertEquals(request.status_code,status.HTTP_200_OK)
+
+    def test_medicos(self): 
+        request = requests.get('http://localhost:8000/medicos/')
+        self.assertEquals(request.status_code,status.HTTP_200_OK)
