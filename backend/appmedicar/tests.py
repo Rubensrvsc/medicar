cd2c1609d422c583cacff4559fafdd71102e52bb
rubensrvsc <Rubensspfc100@gmail.com>
2021-04-21 09:56:32 -0300

    test: add test for endpoint especialidades

--- backend/appmedicar/tests.py
+++ backend/appmedicar/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
-
+from rest_framework import status
+import requests
 # Create your tests here.
+
+class TesteMedicar(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        pass
+
+    def test_especialidades(self):
+        request = requests.get('http://localhost:8000/especialidades/')
+        self.assertEquals(request.status_code,status.HTTP_200_OK)
