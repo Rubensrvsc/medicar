0eefc35ac68d02c1180d376a211e4f48f8abd79e
rubensrvsc <Rubensspfc100@gmail.com>
2021-04-22 08:10:41 -0300

    test: add HTTP_AUTHORIZATION in tests

--- backend/appmedicar/tests.py
+++ backend/appmedicar/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APIRequestFactory, APIClient
 import requests
+from rest_framework.authtoken.models import Token
 # Create your tests here.
 
 class TesteMedicar(TestCase):
@@ -10,20 +11,30 @@ class TesteMedicar(TestCase):
     def setUpTestData(cls):
         data = {
             "username": "Joao",
+            "email":"Joao@mail.com",
             "password": "JoaoJoao"
         }
-
+        factory = APIClient()
+        factory.post('/user_create/',data=data)
+        
     def test_especialidades(self):
         factory = APIClient()
+        token = Token.objects.get(user__username='Joao')
+        factory.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
         response = factory.get('/especialidades/')
         self.assertEquals(response.status_code,status.HTTP_200_OK)
 
     def test_medicos(self): 
         factory = APIClient()
+        token = Token.objects.get(user__username='Joao')
+        factory.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
         response = factory.get('/medicos/')
         self.assertEquals(response.status_code,status.HTTP_200_OK)
     
     def test_agendas(self):
         factory = APIClient()
+        token = Token.objects.get(user__username='Joao')
+        factory.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
         response = factory.get('/agendas/')
         self.assertEquals(response.status_code,status.HTTP_200_OK)
+    
