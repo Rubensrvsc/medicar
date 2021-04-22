5f7f379e3a068598438c558b07beb4104715ef1b
rubensrvsc <Rubensspfc100@gmail.com>
2021-04-22 20:11:51 -0300

    test: add test_marcar_consulta_agenda_dia_passada

--- backend/appmedicar/tests.py
+++ backend/appmedicar/tests.py
@@ -37,12 +37,17 @@ class TesteMedicar(TestCase):
 
         h1 = Horario.objects.get(id=1)
         h2 = Horario.objects.get(id=2)
+        h3 = Horario.objects.get(id=3)
 
         m1 = Medico.objects.get(id=1)
 
         a=Agenda.objects.create(dia='2021-04-25',medico=m1)
         a.horario.add(h1)
         a.horario.add(h2)
+
+        a2=Agenda.objects.create(dia='2021-04-18',medico=m1)
+        a2.horario.add(h2)
+        a2.horario.add(h3)
         
     def test_especialidades(self):
         factory = APIClient()
@@ -125,3 +130,14 @@ class TesteMedicar(TestCase):
         factory.post('/agendar_consulta/',data)
         response = factory.delete('/desmarcar_consulta/1')
         self.assertEquals(response.status_code,status.HTTP_200_OK)
+
+    def test_marcar_consulta_agenda_dia_passada(self):
+        factory = APIClient()
+        token = Token.objects.get(user__username='Joao')
+        factory.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+        data = {
+            'agenda': 2,
+            'horario': '18:00'
+        }
+        response = factory.post('/agendar_consulta/',data)
+        self.assertEquals(response.status_code,status.HTTP_401_UNAUTHORIZED)
