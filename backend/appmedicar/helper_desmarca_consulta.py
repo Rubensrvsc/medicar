8fc72a889c0903e4d83df0dd0e7fa520ad995a2c
rubensrvsc <Rubensspfc100@gmail.com>
2021-05-21 11:04:29 -0300

    refactor: desmarcar_consulta method

--- backend/appmedicar/helper_desmarca_consulta.py
+++ backend/appmedicar/helper_desmarca_consulta.py
@@ -6,17 +6,22 @@ from rest_framework import status
 
 def desmarcar_consulta(id,username):
     consulta = Consulta.objects.filter(id=id)
-    if consulta.exists():
-        consulta_usuario=consulta.filter(Q(cliente__username=username) & Q(id=id))
-        if consulta_usuario.exists():
-            consulta_horario = consulta_usuario.filter(Q(agenda__dia__lt=localdate())  & Q(isMarcada=True)
+    if consulta.exists() == False:
+        return Response({"Erro":"Essa consulta não existe"},status=status.HTTP_404_NOT_FOUND)
+    
+    consulta_usuario=consulta.filter(Q(cliente__username=username) & Q(id=id))
+    
+    if consulta_usuario.exists() == False:
+        return Response({'Erro':'Essa consulta não foi marcada pelo usuário logado'},status=status.HTTP_404_NOT_FOUND)
+    
+    
+    consulta_horario = consulta_usuario.filter(Q(agenda__dia__lt=localdate())  & Q(isMarcada=True)
                 | Q(agenda__dia=localdate()) & Q(horario__hora__lte=localtime()) & Q(isMarcada=True) )
                 
-            if consulta_horario.exists():
-                return Response({'Erro':'consulta já passou'},status=status.HTTP_404_NOT_FOUND)
+    if consulta_horario.exists():
+        return Response({'Erro':'consulta já passou'},status=status.HTTP_404_NOT_FOUND)
 
 
-            return Response(consulta_usuario.filter(id=id).first().delete(),status=status.HTTP_200_OK)
+    return Response(consulta_usuario.filter(id=id).first().delete(),status=status.HTTP_200_OK)
             
-        return Response({'Erro':'Essa consulta não foi marcada pelo usuário logado'},status=status.HTTP_404_NOT_FOUND)
-    return Response({'Erro':'Essa consulta não existe'},status=status.HTTP_404_NOT_FOUND)
+    
